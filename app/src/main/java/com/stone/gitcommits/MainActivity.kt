62c26fa831901e9cd6c18978a44491e8269008cc
awaiss400 <testingbscs@gmail.com>
2022-09-07 19:55:04 +0500

    second feautre comit
     Conflicts:
    	app/src/main/java/com/stone/gitcommits/MainActivity.kt

--- app/src/main/java/com/stone/gitcommits/MainActivity.kt
+++ app/src/main/java/com/stone/gitcommits/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //second commits
         //feture first commit
+        //featuer 2nd
         //MASTER THIRD COMIT
     }
 }
